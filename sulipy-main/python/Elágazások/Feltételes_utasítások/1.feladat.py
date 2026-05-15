2c890d1df9c233f34c16c607f3520f844f5499d6
IllyesBenedek <benedekillyes@gmail.com>
2026-05-15 22:20:13 +0200

    felteteles utasitasok 1 feladat

---
+++ sulipy-main/python/Elágazások/Feltételes_utasítások/1.feladat.py
@@ -0,0 +1,8 @@
+valasz = (input("Jó napod van ma (igen/nem): "))
+
+if valasz == "igen":
+    print("Örülök neki szép napot kivánok")
+elif valasz == "nem":
+    print("Sajnálom remélem holnap jobb napod lesz")
+else:
+    print("Sajnos nem értem a válaszodat")
