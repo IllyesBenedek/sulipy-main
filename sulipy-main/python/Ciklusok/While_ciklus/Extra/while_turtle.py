69d152e7bf43f52f04afa3fb260651aa86fdcd4f
IllyesBenedek <benedekillyes@gmail.com>
2026-05-20 17:53:39 +0200

    turtle

---
+++ sulipy-main/python/Ciklusok/While_ciklus/Extra/while_turtle.py
@@ -0,0 +1,7 @@
+import turtle
+
+r = 50
+
+while r > 10:
+    turtle.circle(r)
+    r -= 10
---
+++ sulipy-main/python/Ciklusok/While_ciklus/Extra/while_turtle2.py
@@ -0,0 +1,8 @@
+import turtle
+
+lepes = 1
+
+while lepes <= 5:
+    turtle.forward(200)
+    turtle.right(72)
+    lepes += 1
