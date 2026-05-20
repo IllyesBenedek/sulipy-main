5733b263e31f033aa1f154942385a7367e9e8b04
IllyesBenedek <benedekillyes@gmail.com>
2026-05-20 21:05:17 +0200

    js alapok

---
+++ sulipy-main/JavaScript/2.1 Alapok/main.js
@@ -0,0 +1,51 @@
+"use strict"; // 2.3 The modern mode, "use strict"
+
+// 2.1 Hello, world!
+console.log("Hello Word!");
+
+// 2.2 Code structure
+console.log("Hello");
+console.log("Word!");
+
+// 2.4 Variables
+let uzenet = "Szia!";
+console.log(uzenet);
+
+uzenet = "Módosított üzenet";
+console.log(uzenet);
+
+const PI = 3.1415;
+console.log(PI);
+
+let elso = "Alma";
+let masodik;
+masodik = elso;
+console.log(masodik);
+
+// 2.5 Data types
+let szam = 42;
+let szoveg = "JavaScript";
+let logikai = true;
+let ures = null;
+let nincsMegadva = undefined;
+
+console.log(typeof szam);
+console.log(typeof szoveg);
+
+// 2.6 Interaction: alert, prompt, confirm
+alert("Üdvözöllek az oldalon!");
+let nev = prompt("Hogy hívnak?", "Vendég");
+console.log(nev);
+let jovahagyas = confirm("Elfogadod a feltételeket?");
+console.log(jovahagyas);
+
+// 2.7 Type Conversions
+let szovegesSzam = "100";
+let igaziSzam = Number(szovegesSzam);
+console.log(typeof igaziSzam);
+
+let logikaiErtek = Boolean(1);
+console.log(logikaiErtek);
+
+let szovegErtek = String(false);
+console.log(typeof szovegErtek);
