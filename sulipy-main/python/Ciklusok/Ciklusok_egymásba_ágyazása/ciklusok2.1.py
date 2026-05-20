471b61a54606c6caf1e30e30458b09d5281c96d2
IllyesBenedek <benedekillyes@gmail.com>
2026-05-20 17:42:46 +0200

    ciklusok egymásba ágyazása

---
+++ sulipy-main/python/Ciklusok/Ciklusok_egymásba_ágyazása/ciklusok2.1.py
@@ -0,0 +1,14 @@
+darab = 1
+
+sor = 1
+
+while sor <= 4:
+
+    oszlop = 1
+
+    while oszlop <= darab:
+        print("O ", end="")
+        oszlop += 1
+    print("")
+    darab += 1
+    sor += 1
---
+++ sulipy-main/python/Ciklusok/Ciklusok_egymásba_ágyazása/ciklusok2.py
@@ -0,0 +1,11 @@
+sor = 1
+
+while sor <= 4:
+
+    oszlop = 1
+
+    while oszlop <= 7:
+        print("O ", end="")
+        oszlop += 1
+    print("")
+    sor += 1
