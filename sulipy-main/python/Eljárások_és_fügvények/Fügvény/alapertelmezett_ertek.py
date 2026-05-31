0310fb978f77eb712ea8a9d5c80e3b0044eae362
IllyesBenedek <benedekillyes@gmail.com>
2026-05-31 13:32:43 +0200

    fugvenyek

---
+++ sulipy-main/python/Eljárások_és_fügvények/Fügvény/alapertelmezett_ertek.py
@@ -0,0 +1,5 @@
+def szamolo(a, b, c=100):
+    return (a + b) * c
+
+
+print(szamolo(1, 2, 1000))
---
+++ sulipy-main/python/Eljárások_és_fügvények/Fügvény/fugvenyek_csere.py
@@ -0,0 +1,5 @@
+def szamolo(a, b, c=100):
+    return (a + b) * c
+
+
+print(szamolo(c=1, b=2, a=1000))
