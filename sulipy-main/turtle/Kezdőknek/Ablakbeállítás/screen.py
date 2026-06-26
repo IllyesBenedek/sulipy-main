da7ed491b2edfb86a38d1af503d518e5b7a34425
IllyesBenedek <benedekillyes@gmail.com>
2026-06-26 11:56:33 +0200

    ablakbeálitás

---
+++ sulipy-main/turtle/Kezdőknek/Ablakbeállítás/screen.py
@@ -0,0 +1,11 @@
+import turtle
+import time
+
+turtle.setup(width=600, height=600)
+turtle.bgcolor("blue")
+turtle.title("Teknőc")
+turtle.bgpic("turtle.gif")
+turtle.forward(500)
+
+time.sleep(2)
+turtle.done()
