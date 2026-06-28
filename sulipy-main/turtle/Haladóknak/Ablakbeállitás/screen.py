037219dbcacf7ccf6e4232b5331c3719f993bd55
IllyesBenedek <benedekillyes@gmail.com>
2026-06-28 16:59:32 +0200

    ablakbeálitás

---
+++ sulipy-main/turtle/Haladóknak/Ablakbeállitás/screen.py
@@ -0,0 +1,14 @@
+import turtle
+import time
+# screen = turtle.Screen()
+# screen.setup(width=200, height=200, startx=0, starty=0)
+# turtle.setup(width=0.75, height=0.5, startx=10, starty=-10)
+# turtle.screensize(canvwidth=1500, canvheight=750, bg="yellow")
+turtle.bgcolor("blue")
+turtle.title("Teknőc")
+turtle.bgpic("turtle.gif")
+turtle.forward(500)
+
+time.sleep(2)
+turtle.clear()
+time.sleep(2)
