7afafbc999e473ba68976916d53f8ee230816885
IllyesBenedek <benedekillyes@gmail.com>
2026-07-02 21:47:31 +0200

    bird

---
+++ sulipy-main/game/Animáció/Képek/1.feladat.py
@@ -0,0 +1,42 @@
+import pygame
+
+
+# --- Beállítások ---
+WIDTH, HEIGHT = 800, 400
+BG_COLOR = (140, 137, 246)
+BIRD_SPEED = 5
+BIRD_WIDTH, BIRD_HEIGHT = 80, 60
+
+# --- Inicializálás ---
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+bird_surf = pygame.image.load("Animáció/Képek/img/bird1.png").convert_alpha()
+bird_surf = pygame.transform.scale(bird_surf, (BIRD_WIDTH, BIRD_HEIGHT))
+bird_x = 0
+bird_y = 200
+
+# --- Fő ciklus ---
+running = True
+while running:
+
+    # --- Események kezelése ---
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # --- Mozgás és ütközésvizsgálat ---
+    bird_x += BIRD_SPEED
+
+    if bird_x + BIRD_WIDTH >= WIDTH:
+        bird_x = WIDTH - BIRD_WIDTH
+
+    # --- Kirajzolás ---
+    screen.fill(BG_COLOR)
+    screen.blit(bird_surf, (bird_x, bird_y))
+    pygame.display.update()
+    clock.tick(60)
+
+
+# --- Kilépés ---
+pygame.quit()
