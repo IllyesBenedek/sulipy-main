4d910d70ce1882660b648bdc23b4a39fa8006ba5
IllyesBenedek <benedekillyes@gmail.com>
2026-07-06 16:13:41 +0200

    1 -2 feladat  felhő beuszása

---
+++ sulipy-main/game/Irányítás_és_ütközés/Irányítás_billentyűzettel/2.feladat.py
@@ -0,0 +1,97 @@
+import pygame
+import random
+
+
+# --- Beállítások ---
+WIDTH = 1280
+HEIGHT = 620
+SPEED = 5
+CLOUD_COUNT = 6
+CLOUD_SPEED = 0.5
+
+
+# --- Inicializálás ---
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+
+bird_fw_1 = pygame.image.load("Irányítás_és_ütközés/Irányítás_billentyűzettel/img/bird1.png").convert_alpha()
+bird_fw_2 = pygame.image.load("Irányítás_és_ütközés/Irányítás_billentyűzettel/img/bird2.png").convert_alpha()
+bird_fw_3 = pygame.image.load("Irányítás_és_ütközés/Irányítás_billentyűzettel/img/bird3.png").convert_alpha()
+bird_fw_4 = pygame.image.load("Irányítás_és_ütközés/Irányítás_billentyűzettel/img/bird4.png").convert_alpha()
+birds_fw = [bird_fw_1, bird_fw_2, bird_fw_3, bird_fw_4]
+bird_b_1 = pygame.image.load("Irányítás_és_ütközés/Irányítás_billentyűzettel/img/bird1back.png").convert_alpha()
+bird_b_2 = pygame.image.load("Irányítás_és_ütközés/Irányítás_billentyűzettel/img/bird2back.png").convert_alpha()
+bird_b_3 = pygame.image.load("Irányítás_és_ütközés/Irányítás_billentyűzettel/img/bird3back.png").convert_alpha()
+bird_b_4 = pygame.image.load("Irányítás_és_ütközés/Irányítás_billentyűzettel/img/bird4back.png").convert_alpha()
+birds_b = [bird_b_1, bird_b_2, bird_b_3, bird_b_4]
+
+cloud_surf = pygame.image.load("Irányítás_és_ütközés/Irányítás_billentyűzettel/img/cloud.png").convert_alpha()
+cloud_positions = []
+for i in range(CLOUD_COUNT):
+    cloud_x = random.randint(0, WIDTH)
+    cloud_y = random.randint(0, HEIGHT)
+    cloud_positions.append([cloud_x, cloud_y])
+
+bird_x = WIDTH / 2
+bird_y = HEIGHT / 2
+bird_index = 0
+
+bird_x = WIDTH / 2
+bird_y = HEIGHT / 2
+bird_rect = birds_fw[bird_index].get_rect(center=(bird_x, bird_y))
+
+counter = 0
+forward = True
+running = True
+
+
+# --- Fő ciklus ---
+while running:
+
+    # --- Események kezelése ---
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_RIGHT] and bird_rect.right <= WIDTH:
+        forward = True
+        bird_x += SPEED
+    elif keys[pygame.K_LEFT] and bird_rect.left >= 0:
+        forward = False
+        bird_x -= SPEED
+
+    if keys[pygame.K_UP] and bird_rect.top >= 0:
+        bird_y -= SPEED
+    elif keys[pygame.K_DOWN] and bird_rect.bottom <= HEIGHT:
+        bird_y += SPEED
+
+    counter += 1
+    if counter % 7 == 0:
+        bird_index += 1
+    if bird_index > len(birds_fw) - 1:
+        bird_index = 0
+
+    # --- Kirajzolás ---
+    screen.fill((140, 137, 246))
+
+    for cloud_pos in cloud_positions:
+        cloud_pos[0] += CLOUD_SPEED
+        if cloud_pos[0] > WIDTH:
+            cloud_pos[0] = -cloud_surf.get_width()
+        screen.blit(cloud_surf, cloud_pos)
+
+    if forward:
+        bird_rect = birds_fw[bird_index].get_rect(center=(bird_x, bird_y))
+        screen.blit(birds_fw[bird_index], bird_rect)
+    else:
+        bird_rect = birds_b[bird_index].get_rect(center=(bird_x, bird_y))
+        screen.blit(birds_b[bird_index], bird_rect)
+
+    pygame.display.update()
+    clock.tick(60)
+
+
+# --- Kilépés ---
+pygame.quit()
